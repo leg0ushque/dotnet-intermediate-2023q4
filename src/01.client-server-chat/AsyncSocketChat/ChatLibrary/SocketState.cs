427186fd949f89b057902057fab5c8d8949afbca
VZ <118164566+leg0ushque@users.noreply.github.com>
2023-11-07 18:30:04 +0300

    Optional homework - async chatserver & client (#2)
    * Create a basic helloworld implementation of solution
    
    * Update implementation of async chat server&client

---
+++ src/01.client-server-chat/AsyncSocketChat/ChatLibrary/SocketState.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+
+namespace AsyncSocketChat.ChatLibrary
+{
+    public class SocketState
+    {
+        public SocketState(Socket socket, byte[] buffer)
+        {
+            Socket = socket;
+            Buffer = buffer;
+        }
+
+        public Socket Socket { get; set; }
+        public byte[] Buffer { get; set; }
+    }
+}
