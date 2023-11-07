e7f196bf1956e76435ef92296a9dcdd8b1a29cc7
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2023-11-07 18:41:38 +0300

    Implement hw2 task 1

--- src/02.async-await/AsyncAwait.Task1.CancellationTokens/Calculator.cs
+++ src/02.async-await/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -1,21 +1,44 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwait.Task1.CancellationTokens;
 
 internal static class Calculator
 {
-    // todo: change this method to support cancellation token
-    public static long Calculate(int n /*, CancellationToken token*/)
+    public static async Task<long> CalculateAsync(int n, CancellationToken token)
     {
         long sum = 0;
 
         for (var i = 0; i < n; i++)
         {
-            // i + 1 is to allow 2147483647 (Max(Int32)) 
+            // i + 1 is to allow 2147483647 (Max(Int32))
             sum = sum + (i + 1);
-            Thread.Sleep(10);
+
+            try
+            {
+                var wh = token.WaitHandle;
+
+                await Task.Delay(10, token);
+            }
+            catch(ObjectDisposedException)
+            {
+                NotifyCalculationProcessCanceled(n);
+                throw new OperationCanceledException("The operation cancelled because of CancellationTokenSource disposed.", token);
+            }
+
+            if (token.IsCancellationRequested)
+            {
+                NotifyCalculationProcessCanceled(n);
+                token.ThrowIfCancellationRequested();
+            }
         }
 
         return sum;
+    }
+
+    private static void NotifyCalculationProcessCanceled(int n)
+    {
+        Console.WriteLine($"Calculation CANCELLED for n={n}");
     }
 }
