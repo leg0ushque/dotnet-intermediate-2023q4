d78d161e5a17fb64fe66b7ecabdca27f0fc3b352
Vadzim Zemchonak <vadzim_zemchonak@epam.com>
2023-11-16 11:00:18 +0300

    Update processing service

--- src/03.message-queues/Services/ProcessingService/ProcessingService.cs
+++ src/03.message-queues/Services/ProcessingService/ProcessingService.cs
@@ -21,6 +21,8 @@ namespace ProcessingService
         {
             MessageParts.Add(new MessagePartialContent(message.Key, message.Value.Content, message.Value.ChunkSize, message.Value.Position));
 
+            _logger.Information($"Message {message.Key} was added to message parts.");
+
             if (message.Value.ChunkSize - 1 == message.Value.Position)
             {
                 SaveToLocalFolder(message.Key);
@@ -30,11 +32,11 @@ namespace ProcessingService
         public void SaveToLocalFolder(string key)
         {
             var fullContent = new List<byte>();
-            var documents = MessageParts.Where(d => d.FileName == key).OrderBy(d => d.Position).ToList();
+            var similarMessageParts = MessageParts.Where(d => d.FileName == key).OrderBy(d => d.Position).ToList();
 
-            for (int i = 0; i < documents.Count(); i++)
+            foreach(var similarMessage in similarMessageParts)
             {
-                fullContent.AddRange(documents[i].Content);
+                fullContent.AddRange(similarMessage.Content);
             }
 
             File.WriteAllBytes($"{Constants.FolderBasePath}{Constants.LocalSavesFolderName}\\{key}", fullContent.ToArray());
